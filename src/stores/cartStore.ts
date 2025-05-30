a6c0a402ef3bf776fd0e951776db7d6c8574d1f3
driardshala <106924402+driardshala@users.noreply.github.com>
2025-05-31 00:02:18 +0200

    Revert "update"
    This reverts commit 4999d811c4417b4f7dab4a12cf081d53f5c3a3f9.

--- src/stores/cartStore.ts
+++ src/stores/cartStore.ts
@@ -1,29 +1,35 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
-interface Book {
+type Book = {
   id: string;
   title: string;
   author: string;
   price: number;
   image: string;
-}
+};
 
-interface CartStore {
+type CartStore = {
   cart: Book[];
   addToCart: (book: Book) => void;
   removeFromCart: (id: string) => void;
-  clearCart: () => void; // <-- Add this
-}
+};
 
-export const useCartStore = create<CartStore>((set) => ({
-  cart: [],
-  addToCart: (book) =>
-    set((state) => ({
-      cart: [...state.cart, book],
-    })),
-  removeFromCart: (id) =>
-    set((state) => ({
-      cart: state.cart.filter((book) => book.id !== id),
-    })),
-  clearCart: () => set({ cart: [] }), // <-- Implement this
-}));
+export const useCartStore = create<CartStore>()(
+  persist(
+    (set) => ({
+      cart: [],
+      addToCart: (book) =>
+        set((state) => ({
+          cart: [...state.cart, book],
+        })),
+      removeFromCart: (id) =>
+        set((state) => ({
+          cart: state.cart.filter((book) => book.id !== id),
+        })),
+    }),
+    {
+      name: "cart-storage", // emri i key-t në localStorage
+    }
+  )
+);
